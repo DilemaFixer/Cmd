8743a93e06bd47c0c3eded78afffe9887538b3cc
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-07-26 00:48:24 +0200

    refactor code

--- parser/parser.go
+++ parser/parser.go
@@ -1,22 +1,30 @@
-package parser 
+package parser
 
-import(
-	"strings"
+import (
 	"fmt"
+	"strings"
 )
 
 type ParserInput struct {
-	Command string
+	Command     string
 	Subcommands []string
-	InputFlags []InputFlag
+	InputFlags  []InputFlag
 }
 
 type InputFlag struct {
-	Name string 
+	Name  string
 	Value string
 }
 
-func (f InputFlag)HaveInputValue() bool {
+func NewParserInput(command string) *ParserInput {
+	return &ParserInput{
+		Command:     command,
+		Subcommands: make([]string, 0),
+		InputFlags:  make([]InputFlag, 0),
+	}
+}
+
+func (f InputFlag) HaveInputValue() bool {
 	return f.Value != ""
 }
 
@@ -25,57 +33,34 @@ func ParseInput(input string) (*ParserInput, error) {
 	if !validateInput(input) {
 		return nil, fmt.Errorf("Parsing err: empty or only whitespace in string")
 	}
-		
-	parts := strings.Split(input, " ")
-	partsCount := len(parts)
 
+	parts, partsCount := cutInput(input)
 	if partsCount == 0 {
 		return nil, fmt.Errorf("Parsing err: empty or only whitespace in string")
 	}
-	
-	result := &ParserInput {
-		Command: parts[0],
-		Subcommands: make([]string, 0),
-		InputFlags: make([]InputFlag, 0),
+
+	result := NewParserInput(parts[0])
+	parts = removeFirst(parts, 1)
+
+	if partsCount == 0 {
+		return result, nil
 	}
 
-	parts = parts[1:]
-	partsCount--
-
-	if partsCount != 0 {
-		var canBeSubcommand bool = true
-		for _, str := range parts {
-			if strings.HasPrefix(str, "--"){
-				canBeSubcommand = false
-				str = str[2:]
-				var flag InputFlag
-				if strings.Contains(str, "=") {
-					strParts := strings.Split(str, "=")
-					println(strParts[1])
-					if strings.TrimSpace(strParts[1]) == "" {
-						return nil, fmt.Errorf("Empty seter for %s", strParts[1])
-					}
-					flag = InputFlag {
-						Name: strParts[0],
-						Value: strParts[1],
-					}
-				} else {
-					flag = InputFlag {
-						Name: str,
-						Value: "",
-					}
-				}
-				
-				result.InputFlags = append(result.InputFlags, flag)
-				continue
+	var canBeSubcommand bool = true
+	for _, str := range parts {
+		if isFlag(str) {
+			canBeSubcommand = false
+			flag, err := parseFlag(str)
+			if err != nil {
+				return nil, err
 			}
-
+			result.InputFlags = append(result.InputFlags, flag)
+		} else {
 			if !canBeSubcommand {
 				return nil, fmt.Errorf("Subcommand command %s can't go after flag", str)
 			}
 			result.Subcommands = append(result.Subcommands, str)
 		}
-
 	}
 
 	return result, nil
@@ -85,3 +70,39 @@ func validateInput(input string) bool {
 	return input != "" && input != " "
 }
 
+func cutInput(str string) ([]string, int) {
+	parts := strings.Split(str, " ")
+	return parts, len(parts)
+}
+
+func removeFirst[T any](parts []T, count int) []T {
+	if partsLen := len(parts); partsLen == 0 || partsLen < count {
+		return parts
+	}
+	return parts[count:]
+}
+
+func isFlag(str string) bool {
+	return strings.HasPrefix(str, "--")
+}
+
+func parseFlag(str string) (InputFlag, error) {
+	str = str[2:]
+	var flag InputFlag
+	if strings.Contains(str, "=") {
+		strParts := strings.Split(str, "=")
+		if strings.TrimSpace(strParts[1]) == "" {
+			return flag, fmt.Errorf("Empty seter for %s", strParts[1])
+		}
+		flag = InputFlag{
+			Name:  strParts[0],
+			Value: strParts[1],
+		}
+	} else {
+		flag = InputFlag{
+			Name:  str,
+			Value: "",
+		}
+	}
+	return flag, nil
+}
