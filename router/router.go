ec4a755dd433d06d7086cf15df26b7521c063653
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-07-27 16:29:50 +0200

    Reapply "Update return value of RoutePoint interface and it's children"
    This reverts commit 302d6fdf3664136acdfe3f7be36e25c6204136aa.

--- router/router.go
+++ router/router.go
@@ -2,31 +2,74 @@ package router
 
 import (
 	"fmt"
+	"os"
 
 	ctx "github.com/DilemaFixer/Cmd/context"
 )
 
 type Router struct {
-	cache  map[string]RoutePoint
-	points []RoutePoint
+	cache        map[string]RoutePoint
+	points       map[string]RoutePoint
+	errorHandler func(error, ctx.Context)
 }
 
 type RoutePoint interface {
 	GetName() string
 	Set(RoutePoint) error
-	ProcessAndPush(ctx.Context, RoutingIterator) error
+	ProcessAndPush(ctx.Context, RoutingIterator) (RoutePoint, error)
 }
 
 func NewRouter() *Router {
 	return &Router{
-		cache: make(map[string]RoutePoint),
+		cache:        make(map[string]RoutePoint),
+		errorHandler: basicErrorHandler,
 	}
+}
+
+func basicErrorHandler(err error, ctx ctx.Context) {
+	if err == nil {
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+	os.Exit(1)
+}
+
+func (r *Router) CustomErrorHandler(errorHandler func(error, ctx.Context)) {
+	if errorHandler == nil {
+		return
+	}
+	r.errorHandler = errorHandler
 }
 
 func (r *Router) AddPoint(point RoutePoint) error {
 	if point == nil {
 		return fmt.Errorf("Router building errror: try add nil RoutePoint")
 	}
-	r.points = append(r.points, point)
+	r.points[point.GetName()] = point
 	return nil
+}
+
+func (r *Router) Route(context ctx.Context, itr RoutingIterator) {
+	point, exist := r.points[itr.RouteToString()]
+
+	if exist {
+		if _, err := point.ProcessAndPush(context, itr); err != nil {
+			r.errorHandler(err, context)
+		}
+		return
+	}
+
+	point, exist = r.points[itr.Get()]
+
+	if !exist {
+		r.errorHandler(fmt.Errorf("Router routing error: try route to non-existent point"), context)
+		return
+	}
+
+	point, err := point.ProcessAndPush(context, itr)
+	if err != nil {
+		r.errorHandler(err, context)
+	}
+	r.cache[point.GetName()] = point
 }
