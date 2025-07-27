302d6fdf3664136acdfe3f7be36e25c6204136aa
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-07-27 16:29:24 +0200

    Revert "Update return value of RoutePoint interface and it's children"
    This reverts commit 318de56849eb365acdf42c89a5413f04bf2cb391.

--- router/end_points.go
+++ router/end_points.go
@@ -78,80 +78,9 @@ func (endPoint *EndPoint) GetName() string {
 }
 
 func (endPoint *EndPoint) Set(routePoint RoutePoint) error {
-	return fmt.Errorf("Can't set route point to EndPoint")
+	return fmt.Errorf("not implemented")
 }
 
-func (endPoint *EndPoint) ProcessAndPush(context ctx.Context, itr RoutingIterator) (RoutePoint, error) {
-	if err := validateOptions(endPoint, context); err != nil {
-		return nil, err
-	}
-
-	return endPoint, nil
-}
-
-func validateOptions(endPoint *EndPoint, context ctx.Context) error {
-
-	var throwError bool = false // change name to more readable
-
-	//TODO: refactor this part , move to new function
-	for _, value := range endPoint.groups.groups {
-		if context.IsFlagExist(value.Triger) {
-			if throwError {
-				return nil // DOTO: Throw error if flag must be lonely
-			}
-
-			throwError = value.RequiresSolitude
-			for _, option := range value.Options {
-				var isExist bool
-				if isExist = context.IsFlagExist(option.Name); isExist && option.Required {
-					return nil //TODO: throw error that requared flag is exist
-				}
-
-				if option.Type != Bool && isExist {
-					if !context.IsFlagHaveValue(option.Name) {
-						return nil // TODO throw error that invalid type or value exist
-					}
-				}
-			}
-		}
-	}
-
-	for _, option := range endPoint.options {
-		var isExist bool
-		if isExist = context.IsFlagExist(option.Name); !isExist && option.Required {
-			return nil // DOTO: throwing error
-		}
-
-		if isExist && option.Type != Bool {
-			if !context.IsFlagHaveValue(option.Name) {
-				return nil //TODO: throwing err
-			}
-		}
-	}
-	return nil
-}
-
-func optionTypeValidation(option Option, context ctx.Context, _type OptionType) error {
-	switch _type {
-	case Bool:
-		if context.IsFlagHaveValue(option.Name) {
-			return nil //TODO: thr err
-		}
-	case String:
-		if !context.IsFlagHaveValue(option.Name) {
-			return nil //TODO: thr err
-		}
-	case Int:
-		if _, error := context.GetValueAsInt(option.Name); error != nil {
-			return nil //TODO: format and return err
-		}
-	case Float:
-		if _, error := context.GetValueAsFloat64(option.Name); error != nil {
-			return nil //TODO: format and return err
-		}
-	default:
-		return nil //return undefind
-	}
-
-	return nil
+func (endPoint *EndPoint) ProcessAndPush(ctx.Context, RoutingIterator) error {
+	return fmt.Errorf("not implemented")
 }
