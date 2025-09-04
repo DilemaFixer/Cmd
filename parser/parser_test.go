b5855dc90017118a988cbf5c3dc8a7e5f491bf3f
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-09-04 15:04:44 +0200

    add test for ParserInput func

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -5,6 +5,91 @@ import (
 	"testing"
 )
 
+func TestParseInput_WithCommandSubcommandsAndAllKindOfFlags_ReturnsParsedInput(t *testing.T) {
+	input := "command subcommand second_subcommand --bool_flag --int_flag=10 --string_flag='hello world'"
+	parsedInput, err := ParseInput(input)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	want := ParsedInput{
+		Command:     "command",
+		Subcommands: []string{"subcommand", "second_subcommand"},
+		InputFlags: []InputFlag{
+			{Name: "bool_flag", Value: ""},
+			{Name: "int_flag", Value: "10"},
+			{Name: "string_flag", Value: "hello world"},
+		},
+	}
+
+	if !reflect.DeepEqual(*parsedInput, want) {
+		t.Fatalf("expected %v, got %v", want, parsedInput)
+	}
+}
+
+func TestParseInput_WithEmptyString_ReturnError(t *testing.T) {
+	_, err := ParseInput("")
+
+	if err == nil {
+		t.Fatalf("expected parsing error")
+	}
+
+	if err.Error() != "Parsing err: empty or only whitespace in string" {
+		t.Fatalf("expected error msg 'Parsing err: empty or only whitespace in string' , got %s", err.Error())
+	}
+}
+
+func TestParseInput_WithSpaceOnlyString_ReturnError(t *testing.T) {
+	_, err := ParseInput("   ")
+
+	if err == nil {
+		t.Fatalf("expected parsing error")
+	}
+
+	if err.Error() != "Parsing err: empty or only whitespace in string" {
+		t.Fatalf("expected error msg 'Parsing err: empty or only whitespace in string' , got %s", err.Error())
+	}
+}
+
+func TestParseInput_WithOnlyCommand_ReturnCommandOnly(t *testing.T) {
+	parsedInput, err := ParseInput("command")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
+	}
+
+	want := ParsedInput{
+		Command:     "command",
+		Subcommands: []string{},
+		InputFlags:  []InputFlag{},
+	}
+
+	if !reflect.DeepEqual(*parsedInput, want) {
+		t.Fatalf("expected %v, got %v", want, parsedInput)
+	}
+}
+
+func TestParseInput_WithFlagOnly_ReturnError(t *testing.T) {
+	_, err := ParseInput("--flag")
+	if err == nil {
+		t.Fatalf("expected error")
+	}
+
+	if err.Error() != "First word must be command , not flag --flag" {
+		t.Fatalf("unexpected error , expect '%s' , get '%s'", "First word must be command , not flag --flag", err.Error())
+	}
+}
+
+func TestParseInput_WithSubcommandAfterFlag_ReturnError(t *testing.T) {
+	_, err := ParseInput("command --flag sucommand")
+	if err == nil {
+		t.Fatalf("expected error")
+	}
+
+	if err.Error() != "Subcommand command sucommand can't go after flag" {
+		t.Fatalf("unexpected error, expect '%s', get '%s'", "Subcommand command sucommand can't go after flag", err.Error())
+	}
+}
+
 func TestParseFlag_WithEmptyString_ReturnError(t *testing.T) {
 	if _, err := parseFlag(""); err != nil && err.Error() != "empty input string" {
 		t.Fatalf("unexpected error , get error: %s", err.Error())
